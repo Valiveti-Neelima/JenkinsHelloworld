652494bc0b478c6138b67f648c274b0da848004d
Valiveti-Neelima <55785702+Valiveti-Neelima@users.noreply.github.com>
2020-10-15 12:45:28 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 public class Helloworld{
  public static void main(String[] args){
-   System.out.println("welcome");
+   System.out.println("Jesus is the reedemer of my sins and sickness.He will save my family from the hell and gives the place in his kingdom to my entire family to stay with him.");
  }
 }
